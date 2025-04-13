9ded2c9b6d5ababc5d01910bd18ccbc3a1d2e70f
schmeckm <markus.schmeckenbecher@gmail.com>
2025-04-13 17:53:12 +0200

    Optimierung von codes

--- src/mqttClientSetup.js
+++ src/mqttClientSetup.js
@@ -1,18 +1,24 @@
+// This code is responsible for initializing the MQTT client.
 const { setupMqttClient } = require('./mqttClient');
 const { oeeLogger, errorLogger, defaultLogger } = require("../utils/logger");
+
 /**
  * Funktion zur Initialisierung des MQTT-Clients.
  * @returns {Object|null} - Gibt den MQTT-Client zurück oder null, wenn die Initialisierung fehlschlägt.
  */
 function initializeMqttClient() {
-    let mqttClient;
+    let mqttClient = null;
     try {
         mqttClient = setupMqttClient();
-        defaultLogger.info('MQTT client initialized successfully.');
+        if (mqttClient) {
+            defaultLogger.info('MQTT client initialized successfully.');
+        } else {
+            errorLogger.error('MQTT client initialization failed (setupMqttClient returned null).');
+        }
     } catch (error) {
         // Logge den gesamten Fehler (einschließlich des Stacktraces), um mehr Kontext zu erhalten
-        errorLogger.error('Error initializing MQTT client:', error);
-        mqttClient = null; // Falls die Initialisierung fehlschlägt, geben wir null zurück
+        errorLogger.error('Error during MQTT client initialization:', error);
+        // mqttClient bleibt null, signalisiert den Fehler
     }
     return mqttClient;
 }
